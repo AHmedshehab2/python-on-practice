3c7572af1660445546d611a8d8ee681027820109
AhmedShehab <shehabah220@gmail.com>
2026-08-20 18:52:49 +0300

    done

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 n = int(input())
 nums = [int(input()) for _ in range(n)]
 # Find and print the second largest unique value
+nums.sort(reverse=True)
+unique_nums = list(set(nums))
+print(unique_nums[1] if len(unique_nums) > 2 else unique_nums[0])
