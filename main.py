4e57ca9fbf9c74a096cae0373c32557a01a1d3cb
AhmedShehab <shehabah220@gmail.com>
2026-09-04 03:53:37 +0300

    done

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-text = input()
-# Count word frequencies and print each in first-seen order
-count = {}
-for word in text.split():
-  count[word] = count.get(word,0) + 1
-for k,y in count.items():
-  print(f"{k} {y}")
+n = int(input())
+students = {}
+for i in range(n):
+    name,grade = input().split()
+    students[name]=grade
+    
+from collections import defaultdict
+
+group = defaultdict(list)
+
+for name , grade in students.items():
+    group[grade].append(name)
+
+for key ,val in dict(group).items():
+    print(f"{key}: {', '.join(val)}")
