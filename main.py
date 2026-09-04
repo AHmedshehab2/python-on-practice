242a131fc84b477e5b105c62b4186a59164be0f7
AhmedShehab <shehabah220@gmail.com>
2026-09-04 04:08:28 +0300

    done

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-n = int(input())
-students = {}
-for i in range(n):
-    name,grade = input().split()
-    students[name]=grade
-    
-from collections import defaultdict
-
-group = defaultdict(list)
-
-for name , grade in students.items():
-    group[grade].append(name)
-
-for key ,val in dict(group).items():
-    print(f"{key}: {', '.join(val)}")
+set1 = set(map(int , input().split()))
+set2 = set(map(int , input().split()))
+intersection= set1 & set2
+for i in intersection:
+    print(i)
