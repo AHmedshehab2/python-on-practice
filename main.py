d34777750deb8c31b35d98720e55dcce8e2f55ee
AhmedShehab <shehabah220@gmail.com>
2026-09-04 04:29:14 +0300

    done

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
 def is_plaindorme(x :str)-> bool:
-    left, right = 0, len(s) - 1
-
-    while left < right:
-        # Move pointers inward if non-alphanumeric
-        while left < right and not s[left].isalnum():
-            left += 1
-        while left < right and not s[right].isalnum():
-            right -= 1
-
-        if s[left].lower() != s[right].lower():
-            return False
-
-        left += 1
-        right -= 1
-
-    return True
-            
+    return x == x[::-1]
 
 
 x = input()
